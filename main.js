fb597d177d215fe8e491589f91ab117f80b9dff3
varannchik <varannchik@gmail.com>
2019-07-06 18:15:25 +0300

    commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let number=document.querySelectorAll('.item');
+let arr=[];
+let btnUp=document.querySelector('.btnUp');
+let btnRight=document.querySelector('.btnRight');
+let btnLeft=document.querySelector('.btnLeft');
+let btnDown=document.querySelector('.btnDown');
+
+let max = 4;
+console.log( Math.random() * max );
+
+// function getRandom(2,4) {
+//     return Math.random();
+// }
+
+
+for(i=0; i<number.length; i++){
+    arr.push(number[i].innerText);
+    
+}
+btnUp.addEventListener('click',()=>{
+
+});
+btnRight.addEventListener('click',()=>{
+    
+});
+btnLeft.addEventListener('click',()=>{
+    
+});
+btnDown.addEventListener('click',()=>{
+    
+});
