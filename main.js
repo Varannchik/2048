f3513716d0eaddb898f5c269b31998600a43b42f
varannchik <varannchik@gmail.com>
2019-07-07 18:03:07 +0300

    commit

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
-let arr=[];
+let arr=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 let btnUp=document.querySelector('.btnUp');
 let btnRight=document.querySelector('.btnRight');
 let btnLeft=document.querySelector('.btnLeft');
 let btnDown=document.querySelector('.btnDown');
 let rating=document.querySelector('h1').innerText;
 let number=document.querySelectorAll('.item');
+let x;
 
 //считываем значение с ячеек
 function numberCell(){    
-    for(let i=0; i<number.length; i++){
-        arr.push(+number[i].innerText);         
-    }
+    for(let i=0; i<number.length; i++){       
+        arr[i]===+number[i].innerText;                        
+    } 
+    return arr;   
 }
+numberCell();
 
 //выбираем случайный номер ячейки
 function rand(){
-    let x = Math.floor((Math.random() * 16));       
+    let x = Math.floor((Math.random() * 16));
+    number[x].innerHTML='2';
+    arr[x]=+2;
 }
+rand();
 
 //суммируем строки
 function rowSumUp(){
-    
-
+    for (let i=0; i<4; i++){
+       let b=arr[i]+arr[i+4]+arr[i+8]+arr[i+12];
+       number[i].innerHTML=b;
+    }
+    for(i=4; i<16; i++){
+        number[i].innerHTML='';
+    }
 }
 
 //суммируем столбцы
@@ -29,10 +40,10 @@ function columnSum(){
     
 }
 
-btnUp.addEventListener('click',function (){    
-    numberCell();
+btnUp.addEventListener('click',function (){
+    numberCell();     
+    rowSumUp(); 
     rand();
-
 });
 btnRight.addEventListener('click',function (){
     numberCell();
